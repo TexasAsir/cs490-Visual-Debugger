b1e5a1fab53086189a476f9708fd6b1fd4e13376
Thomas F. Powers <powerst@moore09.cs.purdue.edu>
2014-02-01 17:33:29 -0500

    lex stuff

---
+++ test.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+int main(){
+printf('\n');
+return 0;
+}
---
+++ y.tab.h
@@ -0,0 +1,68 @@
+typedef union
+#ifdef __cplusplus
+	YYSTYPE
+#endif
+	{
+		char   *string_val;
+	} YYSTYPE;
+extern YYSTYPE yylval;
+
+# define NEWLINE 257
+# define GREAT 258
+# define LESS 259
+# define GREATEQUALS 260
+# define ESSEQUALS 261
+# define OPENBRACE 262
+# define CLOSEBRACE 263
+# define SEMICOLON 264
+# define PLUS 265
+# define MINUS 266
+# define DIVIDE 267
+# define MOD 268
+# define PLUSPLUS 269
+# define MINUSMINUS 270
+# define NOT 271
+# define EQUALSEQUALS 272
+# define EQUALS 273
+# define NOTEQUALS 274
+# define PLUSEQUALS 275
+# define MINUSEQUALS 276
+# define MULEQUALS 277
+# define DIVEQUALS 278
+# define COMMA 279
+# define NOTNOT 280
+# define STAR 281
+# define AND 282
+# define OR 283
+# define OPENPAR 284
+# define CLOSEPAR 285
+# define OPENBRACKET 286
+# define CLOSEBRACKET 287
+# define TYPEDEF 288
+# define INCLUDE 289
+# define RETURN 290
+# define BREAK 291
+# define IF 292
+# define ELSE 293
+# define STRUCT 294
+# define DO 295
+# define WHILE 296
+# define FOR 297
+# define STATIC 298
+# define SIZEOF 299
+# define VOID 300
+# define CONTINUE 301
+# define CONST 302
+# define UNSIGNED 303
+# define INT 304
+# define CHAR 305
+# define SHORT 306
+# define LONG 307
+# define FLOAT 308
+# define DOUBLE 309
+# define BAZINGA 310
+# define CHARCHAR 311
+# define WORD 312
+# define IDENTIFIER 313
+# define NOTOKEN 314
+# define LESSEQUALS 315
